effef0eb487735fa34be247fc350a66f1d93e87f
coffeemakr <leak@bk.ru>
2015-12-10 09:12:16 +0100

    tabs instead of spaces for now

--- app/js/app.js
+++ app/js/app.js
@@ -3,40 +3,40 @@
 /* App Module */
 
 var torstatApp = angular.module('torstatApp', [
-  'ngRoute',
-  'torstatControllers',
-  'torstatFilters',
-  'torstatServices'
+	'ngRoute',
+	'torstatControllers',
+	'torstatFilters',
+	'torstatServices'
 ]);
 
 
 torstatApp
-  .config(['$routeProvider',
-    function($routeProvider) {
-      $routeProvider.
-        when('/router/:routerId', {
-          templateUrl: 'partials/router-detail.html',
-          controller: 'RouterDetailCtrl'
-        }).
-        when('/streams', {
-          templateUrl: 'partials/stream-list.html',
-          controller: 'StreamListCtrl'
-        }).
-        when('/circuits', {
-          templateUrl: 'partials/circuit-list.html',
-          controller: 'CircuitListCtrl'
-        }).
-        when('/circuit/:circuitId', {
-          templateUrl: 'partials/circuit-detail.html',
-          controller: 'CircuitDetailCtrl'
-        }).
-        otherwise({
-          redirectTo: '/circuits'
-        });
-    }
-  ])
-  .config(['$resourceProvider', 
-    function($resourceProvider) {
-      $resourceProvider.defaults.stripTrailingSlashes = false;
-    }
-  ]);
+	.config(['$routeProvider',
+		function($routeProvider) {
+			$routeProvider.
+				when('/router/:routerId', {
+					templateUrl: 'partials/router-detail.html',
+					controller: 'RouterDetailCtrl'
+				}).
+				when('/streams', {
+					templateUrl: 'partials/stream-list.html',
+					controller: 'StreamListCtrl'
+				}).
+				when('/circuits', {
+					templateUrl: 'partials/circuit-list.html',
+					controller: 'CircuitListCtrl'
+				}).
+				when('/circuit/:circuitId', {
+					templateUrl: 'partials/circuit-detail.html',
+					controller: 'CircuitDetailCtrl'
+				}).
+				otherwise({
+					redirectTo: '/circuits'
+				});
+		}
+	])
+	.config(['$resourceProvider', 
+		function($resourceProvider) {
+			$resourceProvider.defaults.stripTrailingSlashes = false;
+		}
+	]);
