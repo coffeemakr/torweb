82efc31d4f1922a0dd4801f8b1cdbe485cf5588f
coffeemakr <leak@bk.ru>
2015-12-10 09:00:25 +0100

    cleanup & formatting

--- app/js/app.js
+++ app/js/app.js
@@ -10,49 +10,33 @@ var torstatApp = angular.module('torstatApp', [
 ]);
 
 
-torstatApp.config(['$provide', function($provide){
-        $provide.decorator('$rootScope', ['$delegate', function($delegate){
-
-            Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
-                value: function(name, listener){
-                    var unsubscribe = $delegate.$on(name, listener);
-                    this.$on('$destroy', unsubscribe);
-
-                    return unsubscribe;
-                },
-                enumerable: false
-            });
-
-
-            return $delegate;
-        }]);
-}]);
-
-torstatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/router/:routerId', {
-        templateUrl: 'partials/router-detail.html',
-        controller: 'RouterDetailCtrl'
-      }).
-      when('/streams', {
-        templateUrl: 'partials/stream-list.html',
-        controller: 'StreamListCtrl'
-      }).
-      when('/circuits', {
-        templateUrl: 'partials/circuit-list.html',
-        controller: 'CircuitListCtrl'
-      }).
-      when('/circuit/:circuitId', {
-        templateUrl: 'partials/circuit-detail.html',
-        controller: 'CircuitDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/circuits'
-      });
-  }]);
-
-
-torstatApp.config(function($resourceProvider) {
-  $resourceProvider.defaults.stripTrailingSlashes = false;
-});
+torstatApp
+  .config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/router/:routerId', {
+          templateUrl: 'partials/router-detail.html',
+          controller: 'RouterDetailCtrl'
+        }).
+        when('/streams', {
+          templateUrl: 'partials/stream-list.html',
+          controller: 'StreamListCtrl'
+        }).
+        when('/circuits', {
+          templateUrl: 'partials/circuit-list.html',
+          controller: 'CircuitListCtrl'
+        }).
+        when('/circuit/:circuitId', {
+          templateUrl: 'partials/circuit-detail.html',
+          controller: 'CircuitDetailCtrl'
+        }).
+        otherwise({
+          redirectTo: '/circuits'
+        });
+    }
+  ])
+  .config(['$resourceProvider', 
+    function($resourceProvider) {
+      $resourceProvider.defaults.stripTrailingSlashes = false;
+    }
+  ]);
