044fa19272a92765e1ad22947313f623ef1943b9
coffeemakr <l34k@bk.ru>
2015-12-21 12:19:17 +0100

    more command line arguments

--- torweb/server.py
+++ torweb/server.py
@@ -36,11 +36,20 @@ def main():
     from twisted.python import log
     import sys
     import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--tls", action="store_true", default=False)
+    parser = argparse.ArgumentParser(prog="torweb")
+    parser.add_argument("port", nargs='?', metavar="PORT", default=8082, type=int, help="Port to listen on. Default: 8082")
+    parser.add_argument("--listen-address", metavar="IP", help="USE CAREFULLY! Set the IP-Address on which torweb listens.")
+    parser.add_argument("-t", "--tls", action="store_true", default=False, help="Use TLS encryption.")
+    parser.add_argument("-c", "--config", default="connections.json", help="Set the configuration file.")
+    parser.add_argument("-k", "--private-key", default="key.pem", help="Path to the private key in PEM format for TLS encryption.")
+    parser.add_argument("-C", "--certificate", default="cert.pem", help="Path to the public certificate in PEM format for TLS encryption.")
+    parser.add_argument("-q", "--quiet", help="Don't log messages.", action="store_true")
+    
     args = parser.parse_args()
-    log.startLogging(sys.stdout)
-    #messageStore = message.MessageStore(sys.argv[1])
+    
+    if not args.quiet:
+        log.startLogging(sys.stdout)
+    
     rootFolder = os.path.abspath(os.path.join(os.path.split(__file__)[0], '..'))
     s = server.Site(RootResource(rootFolder))
     if args.tls:
@@ -51,9 +60,9 @@ def main():
             certData += certFile.read()
 
         certificate = ssl.PrivateCertificate.loadPEM(certData)
-        reactor.listenSSL(8083, s, certificate.options(), interface='127.0.0.1')
+        reactor.listenSSL(args.port, s, certificate.options(), interface='127.0.0.1')
     else:
-        reactor.listenTCP(8082, s, interface='127.0.0.1')
+        reactor.listenTCP(args.port, s, interface='127.0.0.1')
     reactor.run()
 
 if __name__ == "__main__":
