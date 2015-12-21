5ce0c5df19fbd721a6d39a2efb0c6a69eeb83ce5
coffeemakr <l34k@bk.ru>
2015-12-21 12:46:11 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Torweb',
+      version='0.0',
+      description='Tor webinterface',
+      url='https://github.com/coffeemakr/torweb',
+      packages=['torweb'],
+     )
