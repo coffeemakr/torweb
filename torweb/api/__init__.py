697b8671d5a780a4760c2f191b876ed5aa257d16
coffeemakr <l34k@bk.ru>
2016-01-03 14:32:15 +0100

    remove trailing whitespace

--- torweb/api/__init__.py
+++ torweb/api/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 '''
-The RESTful API is built with different resources. The module 
+The RESTful API is built with different resources. The module
 :mod:`torweb.api.ressources` contains all these resources.
 '''
